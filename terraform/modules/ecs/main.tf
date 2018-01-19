768aaae93b57fece995f684b72924d991be91f0e
popopanda <leung.jeff1120@gmail.com>
2018-01-18 22:03:22 -0800

    terraform fmt

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -7,20 +7,20 @@ resource "aws_ecs_service" "chgo" {
   cluster         = "${aws_ecs_cluster.ch.id}"
   task_definition = "${aws_ecs_task_definition.chservice.arn}"
   desired_count   = 1
-  launch_type = "FARGATE"
+  launch_type     = "FARGATE"
 
   network_configuration = {
-    subnets = ["${var.public_subnet_ids}"]
+    subnets         = ["${var.public_subnet_ids}"]
     security_groups = ["sg-e7095293"]
   }
 }
 
 resource "aws_ecs_task_definition" "chservice" {
-  family                = "chservice4"
-  container_definitions = "${file("${path.module}/task-definitions/chservice.json")}"
-  network_mode = "awsvpc"
-  execution_role_arn = "arn:aws:iam::202368747691:role/ecsTaskExecutionRole"
-  cpu = "256"
-  memory = "512"
+  family                   = "chservice4"
+  container_definitions    = "${file("${path.module}/task-definitions/chservice.json")}"
+  network_mode             = "awsvpc"
+  execution_role_arn       = "arn:aws:iam::202368747691:role/ecsTaskExecutionRole"
+  cpu                      = "256"
+  memory                   = "512"
   requires_compatibilities = ["FARGATE"]
 }
--- terraform/providers/aws/us-east-1/ch_lamb_demo/main.tf
+++ terraform/providers/aws/us-east-1/ch_lamb_demo/main.tf
@@ -10,7 +10,7 @@ module "vpc" {
 # Bug in Terraform that does not permit "assign public ip"
 
 module "ecs" {
-  source = "../../../../modules/ecs"
-  name   = "ch_lamb_demo"
+  source            = "../../../../modules/ecs"
+  name              = "ch_lamb_demo"
   public_subnet_ids = "${module.vpc.public_subnet_ids}"
 }
